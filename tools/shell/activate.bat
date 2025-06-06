adcb58c50796db6dd8b38bc98ecb9f8e95c7ee72
xenose <xenose@live.com>
2025-06-06 10:53:12 +0900

    inrdev

--- tools/shell/activate.bat
+++ tools/shell/activate.bat
@@ -1,11 +1,12 @@
 @ECHO OFF
 
 SET PRP=%cd%
+SET TP="\tools\shell\"
 @ECHO Using path: %PRP%
 
 REM code
-DOSKEY wh-build=%PRP%\tools\shell\build.bat %PRP%
+DOSKEY wh-build=%PRP%%TP%build.bat %PRP%
 
 REM build systems
-DOSKEY wh-clang=%PRP%\tools\shell\clang.bat %PRP%
-DOSKEY wh-ninja=%PRP%\tools\shell\ninja.bat %PRP%
+DOSKEY wh-clang=%PRP%%TP%\clang.bat %PRP%
+DOSKEY wh-ninja=%PRP%%TP%\shell\ninja.bat %PRP%
