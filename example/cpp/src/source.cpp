3f2a4b78e3c1877cc864f522e14785e94c166b51
xenose <xenose@live.com>
2025-06-13 22:19:22 +0900

    inrdev

--- example/cpp/src/source.cpp
+++ example/cpp/src/source.cpp
@@ -4,7 +4,7 @@
 int main(int arc, char* const* arv) {
 	wh::instance_c engine = wh::instance_c("cpp", arc, arv);
 
-	wh::print("Hello %s!\n").fd(2).out("world");
+	wh::print().format("Hello %s!\n").fd(2).out("world");
 
 	return 0;
 }
