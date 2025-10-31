5e1ed6689b16d6da2c5b112ad63cc490fde39c9f
xenose <xenose@live.com>
2025-10-31 11:57:13 +0900

    inrdev

--- tests/c/src/maths/core.c
+++ tests/c/src/maths/core.c
@@ -3,32 +3,6 @@
 #include<wh/maths/core.h>
 #include<wh/testing/macros.h>
 
-i8 _testing_int_dec_count(i64* failed, i64 result, i64 in_value, i64 in_base) {
-	i64 out = wh_int_dec_count(in_value, in_base);
-
-	if (result != out) {
-		printf("[\033[31m Failed \033[0m ] Return value is [ %li ] wanted [ %li ] --> dec_count(%li, %li)\n", out, result, in_value, in_base);
-		++*failed;
-		return -1;
-	}
-
-	printf("[\033[32m PASSED \033[0m ] Return value is [ %li ] wanted [ %li ] --> dec_count(%li, %li)\n", out, result, in_value, in_base);
-	return 0;
-}
-
-i8 _testing_intpow(i64 result, i64 in_value, i64 in_base) {
-	i64 out = wh_intpow(in_value, in_base);
-
-	if (result != out) {
-		printf("[\033[31m FAILED \033[0m ] Return value is [ %li ] wanted [ %li ] --> pow(%li, %li)\n", out, result, in_value, in_base);
-		return -1;
-	}
-
-	printf("[\033[32m PASSED \033[0m ] Return value is [ %li ] wanted [ %li ] --> pow(%li, %li)\n", out, result, in_value, in_base);
-	return 0;
-
-}
-
 i64 testing_math() {
 	i64 passed = 0;
 	i64 failed = 0;
