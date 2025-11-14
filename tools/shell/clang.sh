3030ef9de5fab7cc229f4a3f413824c808297165
xenose <xenose@live.com>
2025-11-14 20:31:32 +0900

    inrdev

--- tools/shell/clang.sh
+++ tools/shell/clang.sh
@@ -6,10 +6,10 @@ mkdir -pv "${PRP}/build"
 cd "${PRP}/build"
 
 if command -v clang > /dev/null && command -v clang++ > /dev/null; then
-	cmake .. -DCMAKE_C_COMPILER="$(which clang)" -DCMAKE_CXX_COMPILER="$(which clang++)" -DNO_CXX_COMPILER=False
+	cmake .. -DCMAKE_C_COMPILER="$(which clang)" -DCMAKE_CXX_COMPILER="$(which clang++)" -DNO_CXX_COMPILER=FALSE
 elif command -v clang > /dev/null; then
 	echo "[WARNING] clang++ is not installed, running in C only mode" 
-	cmake .. -DCMAKE_C_COMPILER="$(which clang)" -DNO_CXX_COMPILER=True
+	cmake .. -DCMAKE_C_COMPILER="$(which clang)" -DNO_CXX_COMPILER=TRUE
 else
 	echo "[FAILURE] clang is not installed, exiting..."
 	exit 1
--- tools/shell/gcc.sh
+++ tools/shell/gcc.sh
@@ -6,12 +6,12 @@ mkdir -pv "${PRP}/build"
 cd "${PRP}/build"
 
 if command -v gcc > /dev/null && command -v g++ > /dev/null; then
-	cmake .. -DCMAKE_C_COMPILER="$(which gcc)" -DCMAKE_CXX_COMPILER="$(which g++)" -DNO_CXX_COMPILER=False
+	cmake .. -DCMAKE_C_COMPILER="$(which gcc)" -DCMAKE_CXX_COMPILER="$(which g++)" -DNO_CXX_COMPILER=FALSE
 elif command -v gcc > /dev/null && command -v gcc++ > /dev/null; then
-	cmake .. -DCMAKE_C_COMPILER="$(which gcc)" -DCMAKE_CXX_COMPILER="$(which gcc++)" -DNO_CXX_COMPILER=False
+	cmake .. -DCMAKE_C_COMPILER="$(which gcc)" -DCMAKE_CXX_COMPILER="$(which gcc++)" -DNO_CXX_COMPILER=FALSE
 elif command -v gcc > /dev/null; then
 	echo "[WARNING] g++ is not installed, running in C only mode" 
-	cmake .. -DCMAKE_C_COMPILER="$(which gcc)" -DNO_CXX_COMPILER=True
+	cmake .. -DCMAKE_C_COMPILER="$(which gcc)" -DNO_CXX_COMPILER=TRUE
 else
 	echo "[FAILURE] gcc is not installed, exiting..."
 	exit 1
