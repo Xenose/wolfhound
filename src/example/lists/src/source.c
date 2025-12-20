31a8e4ca0b3179b0bae6efcbb188913ba342ac17
xenose <xenose@live.com>
2025-12-21 00:53:34 +0900

    inrdev

--- src/example/lists/src/source.c
+++ src/example/lists/src/source.c
@@ -5,6 +5,7 @@ int main(int arc, char* const* arv) {
 	wh_list_s l = wh_dlist_init_stdlib(sizeof(int));
 
 	wh_sys_list_insert(&l, 0, (int[]){ 20 });
+	wh_sys_list_insert(&l, 0, (int[]){ 40 });
 
 	printf("--> %i\n", *wh_s2_list_get(int, &l, 0));
 
