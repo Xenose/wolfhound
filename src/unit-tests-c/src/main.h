ab46711359bab5543a7f155d52cb76419340a154
xenose <xenose@live.com>
2025-12-24 22:30:50 +0900

    inrdev

--- src/unit-tests-c/src/main.h
+++ src/unit-tests-c/src/main.h
@@ -8,9 +8,10 @@
 
 extern i64 (*test_funcs[TEST_COUNT])(i64* failed, i64* passed);
 
+// Helper to add unit tests to the test suit
 #define ADD_TESTS(__array__) \
 __attribute__((constructor)) \
-static void reg_func_##__LINE__() { \
+static void reg_func_##__FILE__##__LINE__() { \
 	i64 index = 0; \
 	i64 func_count = sizeof(__array__) / sizeof(__array__[0]); \
 \
