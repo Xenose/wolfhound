6a0a2fc4a042a6b99860783d93ae48949412e5a3
xenose <xenose@live.com>
2026-01-01 13:59:14 +0900

    inrdev

--- include/wh-headers/lua.h
+++ include/wh-headers/lua.h
@@ -4,6 +4,7 @@
 #include<wh-common/os.h>
 
 #if (WH_SYSTEM&WH_SYS_FREEBSD)
+	// TODO remove hard coding of lua path in cmake
 	#include<lua54/lua.h>
 	#include<lua54/lualib.h>
 	#include<lua54/lauxlib.h>
