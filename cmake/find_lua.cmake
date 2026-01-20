b3e39038ad67d9d4c1464a4108072f196a6d457d
xenose <xenose@live.com>
2026-01-20 23:03:40 +0900

    inrdev

--- cmake/find_lua.cmake
+++ cmake/find_lua.cmake
@@ -1,6 +1,6 @@
 INCLUDE(${CMAKE_CURRENT_LIST_DIR}/find.cmake)
 
-SET(LIB_NAME_ALTS lua lua5.4 lua54 lua5.3 lua53)
+SET(LIB_NAME_ALTS lua54 lua53 liblua-5.4 liblua-5.3 lua-5.4 lua5.4 lua-5.3 lua5.3 lua.5.1 lua51 lua)
 SET(LIB_HEADER_NAMES lua.h lualib.h)
-SET(INCLUDE_PREFIX lua5.4 lua54 lua5.3 lua53 lua)
+SET(INCLUDE_PREFIX lua5.4 lua5.4 lua5.3 lua5.3 lua lua54 lua53)
 WH_FIND_LIB(Lua)
