aea21152b84a12823be4c3e16db129f94661b45f
xenose <xenose@live.com>
2026-02-09 22:55:45 +0900

    inrdev

---
+++ tools/shell/python/bin/touch.py
@@ -0,0 +1,21 @@
+import os
+import argparse
+
+
+def execute(cmd, args, session, state):
+    parser = argparse.ArgumentParser(
+        prog='touch',
+        description='',
+        epilog='Text')
+
+    parser.add_argument('PATH', nargs=argparse.REMAINDER)
+
+    a = parser.parse_args(args)
+
+    if os.path.exists(a.PATH[0]):
+        # TODO update timestamp
+        print("File exists...")
+    else:
+        with open(a.PATH[0], "w") as fd:
+            pass
+
