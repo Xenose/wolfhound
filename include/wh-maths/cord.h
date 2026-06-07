e2d01d756ec1dc0c43486c149a31092aa22bdec2
xenose <xenose@live.com>
2026-06-07 23:26:02 +0900

    inrdev

--- include/wh-maths/cord.h
+++ include/wh-maths/cord.h
@@ -3,11 +3,13 @@
 
 #include<stdint.h>
 
-#define WH_32_MM_TO_CORD(x) (((int32_t)(x)) << 10)
-#define WH_32_CORD_TO_MM(x) (((int32_t)(x)) >> 10)
+#define WH_CORD_UNIT 10 // 10 means 10 bits giving us 1024 per 1mm
 
-#define WH_64_MM_TO_CORD(x) (((int64_t)(x)) << 10)
-#define WH_64_CORD_TO_MM(x) (((int64_t)(x)) >> 10)
+#define WH_32_MM_TO_CORD(x) (((int32_t)(x)) << WH_CORD_UNIT)
+#define WH_32_CORD_TO_MM(x) (((int32_t)(x)) >> WH_CORD_UNIT)
+
+#define WH_64_MM_TO_CORD(x) (((int64_t)(x)) << WH_CORD_UNIT)
+#define WH_64_CORD_TO_MM(x) (((int64_t)(x)) >> WH_CORD_UNIT)
 
 typedef int32_t cord32_t;
 typedef int64_t cord64_t;
