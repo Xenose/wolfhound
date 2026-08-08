159c7a9d0e698063e97ffed66eefcd193fa717bd
xenose <xenose@live.com>
2026-08-09 06:44:55 +0900

    inrdev

---
+++ src/engine-posix/_windows/_internal/mmap.c
@@ -0,0 +1,10 @@
+#include<wh-posix/_windows/wnt.h>
+
+i64 _wnt_fd_get(_wnt_s* wnt, va_list args) {
+}
+
+i64 _wnt_fd_insert(_wnt_s* wnt, va_list args) {
+}
+
+i64 _wnt_fd_delete(_wnt_s* wnt, va_list args) {
+}
